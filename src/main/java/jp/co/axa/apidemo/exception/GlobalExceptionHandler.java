1c5f8aa14ee8ffcb9e1dc1e5558214ad84adff5d
Mohd Zaved <ts-zaved.mohd@rakuten.com>
2022-03-26 16:58:55 +0900

    * Handled some more exception

--- src/main/java/jp/co/axa/apidemo/exception/GlobalExceptionHandler.java
+++ src/main/java/jp/co/axa/apidemo/exception/GlobalExceptionHandler.java
@@ -2,8 +2,10 @@ package jp.co.axa.apidemo.exception;
 
 import jp.co.axa.apidemo.constants.ApiCodes;
 import jp.co.axa.apidemo.helper.ResponseProvider;
+import org.springframework.dao.InvalidDataAccessApiUsageException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.InvalidMediaTypeException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
@@ -21,6 +24,24 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(MyCustomException.class)
     public ResponseEntity<Object> handleCustomException(MyCustomException ex) {
         return ResponseProvider.fail(ex.getHttpStatus(), ex.getErrorCode(), ex.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    protected ResponseEntity<Object> handleMethodArgumentTypeMismatch(
+            MethodArgumentTypeMismatchException ex) {
+        return ResponseProvider.fail(HttpStatus.BAD_REQUEST, ApiCodes.INVALID_DATA, ex.getMessage());
+    }
+
+    @ExceptionHandler(InvalidDataAccessApiUsageException.class)
+    protected ResponseEntity<Object> handleInvalidDataAccessApiUsage(
+            InvalidDataAccessApiUsageException ex) {
+        return ResponseProvider.fail(HttpStatus.BAD_REQUEST, ApiCodes.INVALID_DATA, ex.getMessage());
+    }
+
+    @ExceptionHandler(InvalidMediaTypeException.class)
+    protected ResponseEntity<Object> handleInvalidMediaType(
+            InvalidMediaTypeException ex) {
+        return ResponseProvider.fail(HttpStatus.BAD_REQUEST, ApiCodes.INVALID_DATA, ex.getMessage());
     }
 
     @Override
